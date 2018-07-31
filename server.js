ded1f4413004ecb3688c3000d6d35f0f39770eab
Pramith Prakash <pprakash2@sapient.com>
2018-07-31 12:36:31 +0530

    review comments

--- server.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-var express = require('express');
-var app = express();
-
-app.use(express.static('public'));
-app.listen(8080, function() { console.log('listening 8080')});
